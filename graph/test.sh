088fba03b59abd6236a7be989563061eeeea494a
Alon Swartz <alon@alonswartz.org>
2023-04-20 14:42:41 +0300

    graph: combined data file and workaround httpserver requirement (cors)

--- graph/test.sh
+++ graph/test.sh
@@ -1,34 +1,29 @@
 #!/bin/sh
 set -e
 
-_nodes_csv() {
+_data_csv_js() {
 cat<<EOF
-id,title
+var nodesCsv = \`id,title
 1,this is 1
 2,this is 2
 3,this is 3
 4,this is 4
 5,this is 5
 6,this is 6
-EOF
-}
+\`
 
-_links_csv() {
-cat<<EOF
-source,target
+var linksCsv = \`source,target
 1,2
 1,3
 1,4
 2,4
 3,4
 4,5
+\`
 EOF
 }
 
 GRAPH_DIR="$(dirname $(realpath $0))"
-_nodes_csv > $GRAPH_DIR/nodes.csv
-_links_csv > $GRAPH_DIR/links.csv
-
-echo "* serve the graph:"
-echo "python3 -m http.server --bind localhost 8888"
-
+_data_csv_js > $GRAPH_DIR/data.csv.js
+echo "* $GRAPH_DIR/data.csv.js"
+echo "* file://${GRAPH_DIR}/index.html"
