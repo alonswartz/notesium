02bd32b0737f2678f06148a782c44aa2ee312b29
Alon Swartz <alon@alonswartz.org>
2023-06-14 14:54:39 +0300

    golang: notecache and list command

--- notesium.go
+++ notesium.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 )
+
+type Note struct {
+	Filename string
+	Title    string
+}
+
+var noteCache map[string]*Note
 
 func main() {
 	helpFlags := map[string]bool{"-h": true, "--help": true, "help": true}
@@ -19,19 +30,80 @@ func main() {
 	}
 
 	switch os.Args[1] {
-	case "new":
-		notesiumNew(notesiumDir)
 	case "home":
 		fmt.Println(notesiumDir)
+	case "new":
+		notesiumNew(notesiumDir)
+	case "list":
+		notesiumList(notesiumDir)
 	default:
 		fatal("unrecognized command: %s", os.Args[1])
 	}
 }
 
-func notesiumNew(notesiumDir string) {
+func notesiumNew(dir string) {
 	epochInt := time.Now().Unix()
 	epochHex := fmt.Sprintf("%x", epochInt)
-	fmt.Printf("%s/%s.md\n", notesiumDir, epochHex)
+	fmt.Printf("%s/%s.md\n", dir, epochHex)
+}
+
+func notesiumList(dir string) {
+	populateCache(dir)
+
+	for _, note := range noteCache {
+		fmt.Printf("%s:1: %s\n", note.Filename, note.Title)
+	}
+}
+
+func populateCache(dir string) {
+	noteCache = make(map[string]*Note)
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Fatalf("Could not read directory: %s\n", err)
+	}
+
+	for _, file := range files {
+		if !file.IsDir() && strings.HasSuffix(file.Name(), ".md") {
+			filename := file.Name()
+			note, err := readNote(dir, filename)
+			if err != nil {
+				log.Fatalf("Could not read note: %s\n", err)
+			}
+			noteCache[filename] = note
+		}
+	}
+}
+
+func readNote(dir string, filename string) (*Note, error) {
+	path := filepath.Join(dir, filename)
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("could not open file: %s", err)
+	}
+	defer file.Close()
+
+	var title string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if title == "" {
+			title = strings.TrimPrefix(line, "# ")
+			break
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	note := &Note{
+		Filename: filename,
+		Title:    title,
+	}
+
+	return note, nil
 }
 
 func getNotesiumDir() (string, error) {
@@ -73,6 +145,7 @@ func usage() {
 Commands:
   new               Print path for a new note
   home              Print path to notes directory
+  list              Print list of notes
 
 Environment:
   NOTESIUM_DIR      Path to notes directory (default: $HOME/notes)
