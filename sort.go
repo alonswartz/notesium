7278d30289176cbe014d0c67bf3fa72a9b60ba03
Alon Swartz <alon@alonswartz.org>
2023-06-20 15:29:42 +0300

    golang: refactor sort related code to sort.go

---
+++ sort.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"sort"
+)
+
+type SortByCtime []*Note
+type SortByMtime []*Note
+type SortByTitle []*Note
+
+func (n SortByCtime) Len() int           { return len(n) }
+func (n SortByCtime) Less(i, j int) bool { return n[i].Ctime.After(n[j].Ctime) }
+func (n SortByCtime) Swap(i, j int)      { n[i], n[j] = n[j], n[i] }
+
+func (n SortByMtime) Len() int           { return len(n) }
+func (n SortByMtime) Less(i, j int) bool { return n[i].Mtime.After(n[j].Mtime) }
+func (n SortByMtime) Swap(i, j int)      { n[i], n[j] = n[j], n[i] }
+
+func (n SortByTitle) Len() int           { return len(n) }
+func (n SortByTitle) Less(i, j int) bool { return n[i].Title < n[j].Title }
+func (n SortByTitle) Swap(i, j int)      { n[i], n[j] = n[j], n[i] }
+
+func getSortedNotes(sortBy string) []*Note {
+	notes := make([]*Note, 0, len(noteCache))
+
+	for _, note := range noteCache {
+		notes = append(notes, note)
+	}
+
+	switch sortBy {
+	case "ctime":
+		sort.Sort(SortByCtime(notes))
+	case "mtime":
+		sort.Sort(SortByMtime(notes))
+	case "alpha":
+		sort.Sort(SortByTitle(notes))
+	}
+
+	return notes
+}
