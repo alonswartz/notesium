99e9026b4d63067602ee0822fa85345c78f5c1dd
Alon Swartz <alon@alonswartz.org>
2023-06-20 15:41:45 +0300

    golang: refactor usage to options.go

--- notesium.go
+++ notesium.go
@@ -23,7 +23,8 @@ var version = "dev"
 func main() {
 	helpFlags := map[string]bool{"-h": true, "--help": true, "help": true}
 	if len(os.Args) < 2 || helpFlags[os.Args[1]] {
-		usage()
+		fmt.Println(usage)
+		os.Exit(1)
 	}
 
 	versionFlags := map[string]bool{"-v": true, "--version": true, "version": true}
@@ -457,41 +458,5 @@ func defaultColor() Color {
 
 func fatal(format string, a ...interface{}) {
 	fmt.Fprintf(os.Stderr, "Fatal: "+format+"\n", a...)
-	os.Exit(1)
-}
-
-func usage() {
-	fmt.Printf(`Usage: %s COMMAND [OPTIONS]
-
-Commands:
-  new               Print path for a new note
-  home              Print path to notes directory
-  list              Print list of notes
-    --color         Color code prefix using ansi escape sequences
-    --labels        Limit list to only label notes (ie. one word title)
-    --orphans       Limit list to notes without outgoing or incoming links
-    --sort=WORD     Sort list by date or alphabetically (ctime|mtime|alpha)
-    --prefix=WORD   Prefix title with date or linked label (ctime|mtime|label)
-    --date=FORMAT   Date format for ctime/mtime prefix (default: 2006-01-02)
-  links [filename]  Print list of links
-    --color         Color code using ansi escape sequences
-    --outgoing      Limit list to outgoing links related to filename
-    --incoming      Limit list to incoming links related to filename
-    --dangling      Limit list to broken links
-  lines             Print all lines of notes (ie. fulltext search)
-    --color         Color code prefix using ansi escape sequences
-    --prefix=title  Prefix each line with note title
-  stats             Print statistics
-    --color         Color code using ansi escape sequences
-    --table         Format as table with whitespace delimited columns
-  graph             Print graph data
-    --href=FORMAT   Node links format (default: file://%%:p:h/%%:t)
-    --encoded-url   Encode graph data in base64 and append to graph file url
-  version           Print version
-
-Environment:
-  NOTESIUM_DIR      Path to notes directory (default: $HOME/notes)
-
-`, filepath.Base(os.Args[0]))
 	os.Exit(1)
 }
