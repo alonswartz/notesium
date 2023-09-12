6058c19c484e5c876ccbc615688c6afeac8cb856
Alon Swartz <alon@alonswartz.org>
2023-09-12 17:37:58 +0300

    web/raw: handle filter selection

--- web/raw/app.js
+++ web/raw/app.js
@@ -4,11 +4,13 @@ var t = `
     <button class="bg-blue-600 text-white p-1 w-16 rounded-lg" @click="openFilter('/api/raw/list?color=true&prefix=label&sort=alpha')">list</button>
     <button class="bg-blue-600 text-white p-1 w-16 rounded-lg" @click="openFilter('/api/raw/links?color=true')">links</button>
     <button class="bg-blue-600 text-white p-1 w-16 rounded-lg" @click="openFilter('/api/raw/lines?color=true&prefix=title')">lines</button>
-    <input class="border border-gray-400 py-1 px-2 w-96 rounded-lg" placeholder="/api/raw/..." v-model="uri" @change="openFilter(uri)" />
+    <input class="border border-gray-400 py-1 px-2 w-96 rounded-lg" placeholder="/api/raw/..." v-model="filterUri" @change="openFilter(filterUri)" />
   </div>
 </div>
 
-<Filter v-if="showFilter" :uri=uri @filter-selection="handleFilterSelection" />
+<pre class="text-sm p-2" v-text="note"></pre>
+
+<Filter v-if="showFilter" :uri=filterUri @filter-selection="handleFilterSelection" />
 `
 
 import Filter from './filter.js'
@@ -16,18 +18,22 @@ export default {
   components: { Filter },
   data() {
     return {
-      uri: '',
+      note: {},
+      filterUri: '',
       showFilter: false,
       filterConfig: {},
     }
   },
   methods: {
     openFilter(uri) {
-      this.uri = uri;
+      this.filterUri = uri;
       this.showFilter = true;
     },
     handleFilterSelection(value) {
       this.showFilter = false;
+      if (value !== null) {
+        this.note = value
+      }
     },
   },
   created () {
--- web/raw/filter.js
+++ web/raw/filter.js
@@ -1,30 +1,32 @@
 var t = `
-<div @keyup.esc="handleClose()" class="fixed inset-0 z-50 overflow-y-auto p-4 sm:p-6 md:p-20" role="dialog" aria-modal="true" >
-  <div @click="handleClose()" class="fixed inset-0 bg-gray-500 bg-opacity-25 transition-opacity" aria-hidden="true"></div>
+<div @keyup.esc="handleSelection(null)" class="fixed inset-0 z-50 overflow-y-auto p-4 sm:p-6 md:p-20" role="dialog" aria-modal="true" >
+  <div @click="handleSelection(null)" class="fixed inset-0 bg-gray-500 bg-opacity-25 transition-opacity" aria-hidden="true"></div>
   <div class="mx-auto flex flex-col w-full h-full transform overflow-hidden rounded-lg bg-white shadow-2xl ring-1 ring-black ring-opacity-5">
     <div class="relative group flex items-center justify-items-center justify-between space-x-2 border-b border-gray-100">
-      <input ref="queryInput" v-model="query" autofocus placeholder="filter..."
+      <input ref="queryInput" v-model="query" autofocus placeholder="filter..." autocomplete="off" spellcheck="false"
         @blur="$refs.queryInput && $refs.queryInput.focus()"
         @keydown.down.prevent="selectDown()"
         @keydown.up.prevent="selectUp()"
         @keydown.ctrl.j.prevent="selectDown()"
         @keydown.ctrl.k.prevent="selectUp()"
+        @keyup.enter.prevent="(filteredItems.length === 0) ? undefined : handleSelection(selected)"
+        @keydown.tab.prevent
         class="h-12 bg-gray-100 w-full border-0 m-2 rounded-lg px-4 text-gray-900 placeholder:text-gray-400 ring-0 focus:outline-none text-sm" />
       <p class="text-sm px-4">{{ filteredItems.length }}/{{ itemsLength }}</p>
     </div>
     <div class="h-full overflow-y-auto grid grid-cols-2 divide-x divide-gray-300">
       <ul role="listbox" class="max-h-full overflow-y-auto py-1 text-sm font-mono text-gray-800 focus:outline-none">
         <template v-for="item, index in filteredItems">
-          <li :id="'item'+index" @click="selected = index" role="option"
+          <li :id="'item'+index" @click="selected = index" @dblclick="handleSelection(index)" role="option"
             :class="(index === selected) ? '!bg-blue-500 text-white' : ''"
             class="hover:bg-indigo-600/10 cursor-pointer select-none px-4 py-1 whitespace-nowrap">
             <b v-if="item.Colored" v-text="item.Colored"></b> {{ item.Content }}
           </li>
         </template>
       </ul>
-      <div class="text-sm font-mono text-gray-800">
+      <div class="max-h-full overflow-y-auto p-1 text-sm font-mono text-gray-800">
         <template v-if="filteredItems.length > 0">
-          <pre v-text="filteredItems[selected].Filename"></pre>
+          <pre v-text="filteredItems[selected].Filename + ':' + filteredItems[selected].Linenum"></pre>
         </template>
       </div>
     </div>
@@ -43,8 +45,8 @@ export default {
     }
   },
   methods: {
-    handleClose() {
-      this.$emit('filter-selection', 'foo');
+    handleSelection(selected) {
+      this.$emit('filter-selection', selected !== null ? this.filteredItems[selected] : null);
     },
     selectUp() {
       if (this.selected !== 0) {
