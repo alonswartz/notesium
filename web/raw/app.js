5a4b2c6f33bb82e92c7ec2fc67180a77d5d9a3f0
Alon Swartz <alon@alonswartz.org>
2023-09-13 12:02:04 +0300

    web/raw: keybinding sequences

--- web/raw/app.js
+++ web/raw/app.js
@@ -6,6 +6,7 @@ var t = `
     <button class="bg-blue-600 text-white p-1 w-16 rounded-lg" @click="openFilter('/api/raw/lines?color=true&prefix=title')">lines</button>
     <input class="border border-gray-400 py-1 px-2 w-96 rounded-lg" placeholder="/api/raw/..." v-model="filterUri" @change="openFilter(filterUri)" />
   </div>
+  <span class="text-xs" v-text="keySequence.join(' ')"></span>
 </div>
 
 <pre class="text-sm p-2" v-text="note"></pre>
@@ -22,6 +23,7 @@ export default {
       filterUri: '',
       showFilter: false,
       filterConfig: {},
+      keySequence: [],
     }
   },
   methods: {
@@ -35,6 +37,48 @@ export default {
         this.note = value
       }
     },
+    handleKeyPress(event) {
+      if (event.target.tagName !== 'BODY') return
+
+      const leaderKey = 'Space'
+      if (event.code == leaderKey) {
+        this.keySequence = [leaderKey];
+        event.preventDefault();
+        setTimeout(() => { this.keySequence = []; }, 2000);
+        return
+      }
+
+      if (this.keySequence[0] == leaderKey) {
+        this.keySequence.push(event.code)
+        event.preventDefault();
+
+        switch(this.keySequence.join(' ')) {
+            case `${leaderKey} KeyN KeyL`:
+                this.openFilter('/api/raw/list?color=true&prefix=label&sort=alpha');
+                break;
+            case `${leaderKey} KeyN KeyC`:
+                this.openFilter('/api/raw/list?color=true&prefix=ctime&sort=ctime');
+                break;
+            case `${leaderKey} KeyN KeyM`:
+                this.openFilter('/api/raw/list?color=true&prefix=mtime&sort=mtime');
+                break;
+            case `${leaderKey} KeyN KeyK`:
+                this.note.Filename
+                  ? this.openFilter('/api/raw/links?color=true&filename=' + this.note.Filename)
+                  : this.openFilter('/api/raw/links?color=true');
+                break;
+            case `${leaderKey} KeyN KeyS`:
+                this.openFilter('/api/raw/lines?color=true&prefix=title');
+                break;
+        }
+      }
+    },
+  },
+  mounted() {
+    document.addEventListener('keydown', this.handleKeyPress);
+  },
+  beforeDestroy() {
+    document.removeEventListener('keydown', this.handleKeyPress);
   },
   created () {
     this.openFilter('/api/raw/list?color=true&prefix=label&sort=alpha');
