821a2382ce4647ba6574b39683a04fc09ca19ac8
Alon Swartz <alon@alonswartz.org>
2023-09-14 11:37:56 +0300

    web/raw: filter preview note linenum highlight and scroll-into-view

--- web/raw/preview.js
+++ web/raw/preview.js
@@ -1,5 +1,5 @@
 var t = `
-<pre class="w-full h-full overflow-auto p-1" v-text="note.Content"></pre>
+<pre class="w-full h-full overflow-auto p-1"><template v-for="line, index in noteLines"><span :id="'line'+(index+1)" v-text="line+'\\n'"></span></template></pre>
 `
 
 export default {
@@ -15,11 +15,28 @@ export default {
         .then(response => response.json())
         .then(data => {
           this.note = data;
+          this.lineNumberHL(this.lineNumber, null);
         });
+    },
+    lineNumberHL(newLineNumber, oldLineNumber) {
+      this.$nextTick(() => {
+        if (oldLineNumber) {
+          const oldLineEl = document.getElementById(`line${oldLineNumber}`);
+          if (oldLineEl) { oldLineEl.classList.remove('bg-blue-600/10'); }
+        }
+        const newLineEl = document.getElementById(`line${newLineNumber}`);
+        if (newLineEl) { newLineEl.classList.add('bg-blue-600/10', 'block', 'w-full'); newLineEl.scrollIntoView({ block: 'nearest' }); }
+      });
+    },
+  },
+  computed: {
+    noteLines() {
+      if (this.note.Content) return this.note.Content.split('\n');
     },
   },
   watch: {
     filename: function() { this.fetchNote(); },
+    lineNumber: function(newVal, oldVal) { this.lineNumberHL(newVal, oldVal); },
   },
   created() {
     this.fetchNote();
