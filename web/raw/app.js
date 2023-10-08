2eb14e068083ed80db4c207323dfae36404ea8dc
Alon Swartz <alon@alonswartz.org>
2023-10-08 15:27:28 +0300

    web/raw: refactored tabs into component

--- web/raw/app.js
+++ web/raw/app.js
@@ -2,26 +2,7 @@ var t = `
 <div class="relative flex flex-col max-h-screen h-screen overflow-y bg-gray-50">
 
   <nav class="flex bg-gray-200 text-gray-800">
-    <div class="flex flex-nowrap max-w-full w-full h-9 overflow-x-hidden items-center content-center px-2 mr-6">
-      <template v-for="note in notes" :key="note.Filename">
-        <div :class="(note.Filename == activeFilename) ? 'text-gray-50' : 'text-transparent'" class="relative h-full">
-          <svg class="absolute right-0 bottom-0" fill="currentColor" width="7" height="7"><path d="M 0 7 A 7 7 0 0 0 7 0 L 7 7 Z"></path></svg>
-        </div>
-        <div @click="activeFilename = note.Filename"
-          :title="note.Title + ' (' + note.Filename + ')'"
-          :class="(note.Filename == activeFilename) ? 'bg-gray-50 text-gray-800' : 'hover:bg-gray-100/75 hover:text-gray-700 text-gray-500'"
-          class="flex rounded-t-lg justify-between basis-52 truncate text-xs h-full items-center pl-3 pr-2 cursor-pointer">
-          <span class="truncate pt-px" v-text="note.Title"></span>
-          <span @click.stop="closeNote(note.Filename)" class="hover:bg-gray-300 hover:rounded-full">
-            <Icon name="mini-x-mark" size="4" />
-          </span>
-        </div>
-        <div :class="(note.Filename == activeFilename) ? 'text-gray-50' : 'text-transparent'" class="relative h-full">
-          <svg class="absolute bottom-0" fill="currentColor" width="7" height="7"><path d="M 0 0 A 7 7 0 0 0 7 7 L 0 7 Z"></path></svg>
-        </div>
-        <span :class="(note.Filename != activeFilename) ? 'text-gray-300' : 'text-transparent'" class="z-1 -mr-1 ">|</span>
-      </template>
-    </div>
+    <Tabs :notes=notes :activeFilename=activeFilename @note-activate="activateNote" @note-close="closeNote" />
 
     <div class="flex w-auto py-2 mt-0 ml-auto items-center space-x-5 pr-5">
       <span title="list" @click="openFilter('/api/raw/list?color=true&prefix=label&sort=alpha');"
@@ -52,9 +33,10 @@ var t = `
 `
 
 import Filter from './filter.js'
+import Tabs from './tabs.js'
 import Icon from './icon.js'
 export default {
-  components: { Filter, Icon },
+  components: { Filter, Tabs, Icon },
   data() {
     return {
       notes: [],
@@ -87,6 +69,9 @@ export default {
           this.activeFilename = note.Filename;
         });
     },
+    activateNote(filename) {
+      this.activeFilename = filename;
+    },
     closeNote(filename) {
       const index = this.notes.findIndex(note => note.Filename === filename);
       if (index === -1) return;
@@ -100,16 +85,6 @@ export default {
     },
     handleKeyPress(event) {
       if (event.target.tagName !== 'BODY') return
-
-      if (event.ctrlKey && (event.code == 'KeyJ' || event.code == 'KeyK')) {
-        const index = this.notes.findIndex(note => note.Filename === this.activeFilename);
-        if (index === -1) return;
-        const movement = event.code === 'KeyJ' ? 1 : -1;
-        const newIndex = (index + movement + this.notes.length) % this.notes.length;
-        this.activeFilename = this.notes[newIndex].Filename;
-        event.preventDefault();
-        return;
-      }
 
       let timeoutId;
       const leaderKey = 'Space'
