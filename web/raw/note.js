1d1dfd5e0de23bc37928dfbe9ab883af7809c2db
Alon Swartz <alon@alonswartz.org>
2023-10-30 16:20:41 +0200

    web/raw: codemirror - initial setup

--- web/raw/note.js
+++ web/raw/note.js
@@ -1,7 +1,7 @@
 var t = `
 <div class="flex h-full w-full overflow-x-auto gap-1 p-2">
   <div class="relative overflow-y-auto w-4/6">
-    <pre class="p-2 font-mono text-gray-800 text-sm" v-text="note.Content"></pre>
+    <div class="p-2" ref="codemirror"></div>
   </div>
   <div class="relative overflow-y-auto w-2/6 rounded-lg border border-gray-200 bg-white">
     <pre class="p-2 font-mono text-gray-800 text-xs" v-text="note"></pre>
@@ -11,5 +11,16 @@ var t = `
 
 export default {
   props: ['note'],
+  mounted() {
+    this.cm = new CodeMirror(this.$refs.codemirror, {
+      value: this.note.Content,
+      lineNumbers: false,
+      theme: 'notesium-light',
+      mode: {
+        name: "markdown",
+      },
+    });
+    this.cm.setSize("100%", "100%");
+  },
   template: t
 }
