bbbf949e0bd2014ca5fec72a8f0adcdccd719a10
Alon Swartz <alon@alonswartz.org>
2023-11-14 14:07:20 +0200

    web/raw: track and visualize note.isModified, save placeholder

--- web/raw/note.js
+++ web/raw/note.js
@@ -4,8 +4,11 @@ var t = `
     <div :class="{ 'conceal': conceal }" class="p-2 h-full" ref="codemirror"></div>
   </div>
   <div class="relative overflow-y-auto w-2/6 rounded-lg border border-gray-200 bg-white">
-    <div class="p-2 border-b">
-      <div class="flex h-4 text-xs items-center content-center">
+    <div class="p-2 border-b flex space-x-4">
+      <button type="button" :disabled="!this.note.isModified" @click="handleSave()"
+        :class="this.note.isModified ? 'bg-blue-600 hover:bg-blue-500 text-white' : 'bg-gray-300 text-gray-400'"
+        class="rounded px-2 pt-px text-xs shadow-sm">Save</button>
+      <div class="flex h-6 text-xs items-center content-center">
         <input type="checkbox" v-model="conceal" id="conceal-checkbox" tabindex="-1" @click="event => event.target.blur()">
         <label for="conceal-checkbox" class="ml-1">conceal</label>
       </div>
@@ -49,6 +52,14 @@ export default {
       }
       this.$nextTick(() => { this.cm.focus(); } );
     },
+    handleSave() {
+      if (this.note.isModified) {
+        // TODO: perform actual save.
+        console.log('note-save', this.note.Filename, this.cm.getValue() );
+        this.note.isModified = false;
+        this.cm.doc.markClean();
+      }
+    },
     lineNumberHL(linenum) {
       this.$nextTick(() => {
         this.cm.setOption("styleActiveLine", true);
@@ -69,6 +80,7 @@ export default {
       extraKeys: {
         "[": this.handleLeftBracket,
         "Esc": function(cm){ cm.display.input.blur(); document.body.focus(); },
+        "Ctrl-S": this.handleSave,
       },
     });
 
@@ -81,6 +93,9 @@ export default {
     });
     this.cm.on('blur', (cm, e) => {
       this.cm.setOption("styleActiveLine", false);
+    });
+    this.cm.on('changes', (cm, changes) => {
+      this.note.isModified = !cm.doc.isClean();
     });
 
     this.cm.on('mousedown', (cm, e) => {
