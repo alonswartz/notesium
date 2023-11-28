1f362717683f3cc094c60e69936a47d09b697159
Alon Swartz <alon@alonswartz.org>
2023-11-28 13:22:28 +0200

    web/raw: note sidebar metadata stats

--- web/raw/note.js
+++ web/raw/note.js
@@ -17,6 +17,24 @@ var t = `
         </a>
       </div>
     </div>
+
+    <div v-if="!note.ghost">
+      <dl class="m-2 grid grid-cols-3 gap-2">
+        <div class="overflow-hidden rounded-lg bg-gray-50 px-4 py-2">
+          <dd class="mt-1 text-sm font-semibold tracking-tight text-gray-900" v-text="note.Lines"></dd>
+          <dt class="text-sm font-medium text-gray-500">Lines</dt>
+        </div>
+        <div class="overflow-hidden rounded-lg bg-gray-50 px-4 py-2">
+          <dd class="mt-1 text-sm font-semibold tracking-tight text-gray-900" v-text="note.Words"></dd>
+          <dt class="text-sm font-medium text-gray-500">Words</dt>
+        </div>
+        <div class="overflow-hidden rounded-lg bg-gray-50 px-4 py-2">
+          <dd class="mt-1 text-sm font-semibold tracking-tight text-gray-900" v-text="note.Chars"></dd>
+          <dt class="text-sm font-medium text-gray-500">Chars</dt>
+        </div>
+      </dl>
+    </div>
+
     <pre class="p-2 font-mono text-gray-800 text-xs" v-text="note"></pre>
   </div>
   <Filter v-if="showFilter" :uri=filterUri small=true @filter-selection="handleFilterSelection" />
