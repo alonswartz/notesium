2072a095fd3fb8c95f827d0656a9b20a8d686e51
Alon Swartz <alon@alonswartz.org>
2023-11-29 16:21:47 +0200

    web/raw: filter preview note leveraging codemirror

--- web/raw/preview.js
+++ web/raw/preview.js
@@ -1,45 +1,40 @@
 var t = `
-<pre class="w-full h-full overflow-auto p-1"><template v-for="line, index in noteLines"><span :id="'line'+(index+1)" v-text="line+'\\n'"></span></template></pre>
+<div class="h-full conceal-noactive" ref="preview"></div>
 `
 
 export default {
   props: ['filename', 'lineNumber'],
-  data() {
-    return {
-      note: {},
-    }
-  },
   methods: {
     fetchNote() {
       fetch("/api/notes/" + this.filename)
         .then(response => response.json())
-        .then(data => {
-          this.note = data;
-          this.lineNumberHL(this.lineNumber, null);
+        .then(note => {
+          this.cm.setValue(note.Content);
+          this.lineNumberHL();
         });
     },
-    lineNumberHL(newLineNumber, oldLineNumber) {
+    lineNumberHL() {
       this.$nextTick(() => {
-        if (oldLineNumber) {
-          const oldLineEl = document.getElementById(`line${oldLineNumber}`);
-          if (oldLineEl) { oldLineEl.classList.remove('bg-blue-600/10'); }
-        }
-        const newLineEl = document.getElementById(`line${newLineNumber}`);
-        if (newLineEl) { newLineEl.classList.add('bg-blue-600/10', 'block', 'w-full'); newLineEl.scrollIntoView({ block: 'nearest' }); }
+        this.cm.setCursor({line: this.lineNumber - 1, ch: 0});
       });
     },
   },
-  computed: {
-    noteLines() {
-      if (this.note.Content) return this.note.Content.split('\n');
-    },
+  mounted() {
+    this.cm = new CodeMirror(this.$refs.preview, {
+      value: '',
+      readOnly: true,
+      styleActiveLine: true,
+      theme: 'notesium-light',
+      mode: {
+        name: "gfm",
+        highlightFormatting: true,
+      },
+    });
+    this.fetchNote();
   },
   watch: {
     filename: function() { this.fetchNote(); },
-    lineNumber: function(newVal, oldVal) { this.lineNumberHL(newVal, oldVal); },
-  },
-  created() {
-    this.fetchNote();
+    lineNumber: function() { this.lineNumberHL(); },
   },
   template: t
 }
