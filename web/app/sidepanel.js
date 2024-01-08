bb3e7de096f403f261181fce90c8a16066703688
Alon Swartz <alon@alonswartz.org>
2024-01-08 16:42:51 +0200

    web/app: sidepanel labels integrated with notes-list

--- web/app/sidepanel.js
+++ web/app/sidepanel.js
@@ -1,16 +1,14 @@
 var t = `
-<div class="hidden flex-none overflow-y-auto w-48 bg-gray-800 text-gray-400 px-2 text-sm font-medium divide-y divide-gray-600">
+<div class="flex-none overflow-y-auto w-48 bg-gray-800 text-gray-400 px-2 text-sm font-medium divide-y divide-gray-700">
   <ul class="space-y-1 cursor-pointer py-2">
-    <li v-for="finder in staticFinders" :key="finder.name" :title="finder.hover"
-      @click="$emit('finder-open', finder.uri)"
-      class="p-2 rounded-md hover:text-gray-100 hover:bg-gray-700">
-      <span class="overflow-hidden truncate pr-2" v-text="finder.title" />
-    </li>
+    <li title="notes sorted alphabetically" @click="sortBy='title', query=''"
+      class="p-2 rounded-md hover:text-gray-100 hover:bg-gray-700">All</li>
+    <li title="notes sorted by modication date" @click="sortBy='mtime', query=''"
+      class="p-2 rounded-md hover:text-gray-100 hover:bg-gray-700">Recent</li>
   </ul>
-  <ul class="space-y-1 cursor-pointer py-1">
-    <li v-show="labels.length == 0" class="p-2 cursor-default" title="1-word titled notes are considered labels">no labels found</li>
+  <ul v-show="labels.length > 0" class="space-y-1 cursor-pointer py-2">
     <li v-for="label in labels" :key="label.Filename"
-      @click="$emit('note-open', label.Filename)"
+      @click="sortBy='title', query=label.Title + ' '"
       class="flex justify-between p-2 rounded-md hover:text-gray-100 hover:bg-gray-700">
       <span class="overflow-hidden truncate pr-2" v-text="label.Title" />
       <span title="links" @click.stop="$emit('finder-open', '/api/raw/links?color=true&filename=' + label.Filename)"
@@ -66,12 +64,6 @@ export default {
       sortBy: 'title',
       notes: [],
       labels: [],
-      staticFinders: [
-        { title: 'all', hover: 'notes sorted alphabetically', uri: '/api/raw/list?color=true&prefix=label&sort=alpha' },
-        { title: 'recent', hover: 'notes sorted by modification date', uri: '/api/raw/list?color=true&prefix=mtime&sort=mtime' },
-        { title: 'orphans', hover: 'notes without incoming or outgoing links', uri: '/api/raw/list?orphans=true&sort=alpha' },
-        { title: 'dangling links', hover: 'notes with broken outgoing links', uri: '/api/raw/links?dangling=true' },
-      ],
     }
   },
   methods: {
