42705e2eea87d51f94ae8c8fb7f6e17f3af62e59
Alon Swartz <alon@alonswartz.org>
2024-01-08 10:46:14 +0200

    web/app: sidepanel finder links - all recent orphans dangling

--- web/app/sidepanel.js
+++ web/app/sidepanel.js
@@ -1,6 +1,13 @@
 var t = `
-<div class="flex-none overflow-y-auto w-48 bg-gray-800 text-gray-400 p-2">
-  <ul class="space-y-1 cursor-pointer text-sm font-medium">
+<div class="flex-none overflow-y-auto w-48 bg-gray-800 text-gray-400 px-2 text-sm font-medium divide-y divide-gray-600">
+  <ul class="space-y-1 cursor-pointer py-2">
+    <li v-for="finder in staticFinders" :key="finder.name" :title="finder.hover"
+      @click="$emit('finder-open', finder.uri)"
+      class="p-2 rounded-md hover:text-gray-100 hover:bg-gray-700">
+      <span class="overflow-hidden truncate pr-2" v-text="finder.title" />
+    </li>
+  </ul>
+  <ul class="space-y-1 cursor-pointer py-1">
     <li v-show="labels.length == 0" class="p-2 cursor-default" title="1-word titled notes are considered labels">no labels found</li>
     <li v-for="label in labels" :key="label.Filename"
       @click="$emit('note-open', label.Filename)"
@@ -23,6 +30,12 @@ export default {
   data() {
     return {
       labels: [],
+      staticFinders: [
+        { title: 'all', hover: 'notes sorted alphabetically', uri: '/api/raw/list?color=true&prefix=label&sort=alpha' },
+        { title: 'recent', hover: 'notes sorted by modification date', uri: '/api/raw/list?color=true&prefix=mtime&sort=mtime' },
+        { title: 'orphans', hover: 'notes without incoming or outgoing links', uri: '/api/raw/list?orphans=true&sort=alpha' },
+        { title: 'dangling links', hover: 'notes with broken outgoing links', uri: '/api/raw/links?dangling=true' },
+      ],
     }
   },
   methods: {
