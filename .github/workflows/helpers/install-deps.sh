18e2b38a40c20fca6e42a61481ebd6775cbbf657
Alon Swartz <alon@alonswartz.org>
2024-04-11 18:05:51 +0300

    gha: install tailwindcss and build web

--- .github/workflows/helpers/install-deps.sh
+++ .github/workflows/helpers/install-deps.sh
@@ -23,14 +23,27 @@ _install_bats_core() {
     sudo ./install.sh /usr/local
 }
 
+_install_tailwindcss() {
+    info "installing tailwindcss ..."
+    URL="https://github.com/tailwindlabs/tailwindcss/releases/download/v3.1.6/tailwindcss-linux-x64"
+    HASH="ebb892a4749798f94a32915d50298c5fac54b6a5143fc49243d37343fe7b9227"
+    curl -sLo $HOME/tailwindcss $URL
+    chmod +x $HOME/tailwindcss
+    echo -n "$HASH  $HOME/tailwindcss" | sha256sum --strict --check -
+    sudo mv $HOME/tailwindcss /usr/local/bin/
+}
+
 main() {
     case $1 in -h|--help|help) usage;; esac
     command -v git >/dev/null || fatal "git not found"
+    command -v curl >/dev/null || fatal "curl not found"
+    command -v sha256sum >/dev/null || fatal "sha256sum not found"
 
     [ -n "$GITHUB_WORKSPACE" ] || fatal "GITHUB_WORKSPACE not set"
     cd "$GITHUB_WORKSPACE"
 
     _install_bats_core
+    _install_tailwindcss
 }
 
 main "$@"
