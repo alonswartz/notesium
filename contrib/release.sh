152230bada7b05e6a7121b5324efc4e7cbe8067a
Alon Swartz <alon@alonswartz.org>
2024-05-01 11:09:33 +0300

    contrib: refactor release related scripts into release.sh

--- contrib/release.sh
+++ contrib/release.sh
@@ -7,8 +7,12 @@ info() { echo "[$SCRIPT_NAME] INFO: $*"; }
 
 usage() {
 cat<<EOF
-Usage: $SCRIPT_NAME vX.Y.Z
-Helper script to tag release version with verification
+Usage: $SCRIPT_NAME COMMAND [ARGS]
+
+Commands:
+  commits       Print commits as reference for new changelog entry
+  build         Mimic gh-action build and test
+  tag SEMVER    Verify, tag release and push to trigger gh-action (vX.Y.Z)
 EOF
 exit 1
 }
@@ -34,16 +38,16 @@ _verify_tag_matches_changelog() {
     return 1
 }
 
-_verify_gitversion_changelog() {
+_verify_gitver_matches_changelog() {
     info "verify gitversion matches changelog ..."
     [ -e "CHANGELOG.md" ] || fatal "CHANGELOG.md not found"
     version_changelog="$(awk 'FNR==1{print $2}' CHANGELOG.md)"
-    version_git="$(git describe | sed 's/^v//; s/-/+/')"
+    version_git="$(git describe --tags | sed 's/^v//; s/-/+/')"
     [ "${version_changelog}" = "${version_git}" ] && return 0
     return 1
 }
 
-_verify_branch() {
+_verify_branch_master() {
     info "verify branch is master ..."
     branch="$(git rev-parse --abbrev-ref HEAD)"
     [ "$branch" = "master" ] && return 0
@@ -80,8 +84,8 @@ _verify_local_remote() {
 
 _verify_actual_remote() {
     info "verify HEAD matches actual remote ..."
-    url="https://api.github.com/repos/alonswartz/notesium/branches/master"
-    remote_sha=$(curl -s "$url" | jq -r '.commit.sha')
+    gh="https://api.github.com/repos/alonswartz/notesium"
+    remote_sha="$(curl -s "$gh/branches/master" | jq -r '.commit.sha')"
     local_sha="$(git rev-parse HEAD)"
     [ "$local_sha" = "$remote_sha" ] && return 0
     echo "local_sha: $local_sha"
@@ -89,19 +93,63 @@ _verify_actual_remote() {
     return 1
 }
 
-main() {
-    case $1 in ''|-h|--help|help) usage;; esac
+print_commits() {
+    export GITHUB_WORKSPACE="$(pwd)"
+    gref="refs/heads/$(git rev-parse --abbrev-ref HEAD)"
+    .github/workflows/helpers/release-notes.sh $gref
+}
+
+build_release() {
+    export GITHUB_WORKSPACE="$(pwd)"
+
+    version_git="$(git describe --tags | sed 's/^v//; s/-/+/')"
+    [ -n "$version_git" ] || fatal "could not determine version_git"
+
+    version_changelog="$(awk 'FNR==1{print $2}' CHANGELOG.md)"
+    [ -n "$version_changelog" ] || fatal "could not determine version_chl"
+
+    outdir="build/$version_git"
+    [ -e "$outdir" ] && fatal "$outdir already exists"
+
+    info "version.git:       $version_git"
+    info "version.changelog: $version_changelog"
+    if [ "${version_git}" = "${version_changelog}" ]; then
+        gref="refs/tags/v${version_git}"
+    else
+        gref="refs/heads/$(git rev-parse --abbrev-ref HEAD)"
+        _ask "WARNING: version mismatch"
+    fi
+
+    _verify_branch_master || _ask "WARNING: branch not master"
+    _verify_branch_clean || _ask "WARNING: branch is dirty"
+
+    info "Build web"
+    ./web/app/make.sh all
+
+    info "Build binaries"
+    .github/workflows/helpers/build-bin.sh $outdir/ all
+
+    info "Run tests"
+    .github/workflows/helpers/run-tests.sh $outdir/notesium-linux-amd64
+
+    info "Generate release notes"
+    .github/workflows/helpers/release-notes.sh $gref > $outdir/release-notes.md
+
+    info "$outdir/"
+    ls -lh $outdir/
+}
+
+tag_release() {
     command -v jq >/dev/null || fatal "jq not found"
-    command -v git >/dev/null || fatal "git not found"
     command -v curl >/dev/null || fatal "curl not found"
-    cd "$(dirname "$(dirname "$(realpath "$0")")")"
 
     REQUESTED_TAG="$1"
+    [ -n "$REQUESTED_TAG" ] || fatal "REQUESTED_TAG not specified"
 
     _verify_branch_clean || _ask "WARNING: branch is dirty"
     _verify_tag_does_not_exist || fatal "tag already exists"
     _verify_tag_matches_changelog || fatal "tag changelog mismatch"
-    _verify_branch || fatal "branch not master"
+    _verify_branch_master || fatal "branch not master"
     _verify_changelog_diff || fatal "changelog has changes"
     _verify_local_remote || fatal "local_remote mismatch"
     _verify_actual_remote || fatal "actual_remote mismatch"
@@ -109,13 +157,25 @@ main() {
     _ask "WARNING: about to create tag: $REQUESTED_TAG"
     git tag "$REQUESTED_TAG" -a -m "$REQUESTED_TAG" || fatal "tagging failed"
 
-    _verify_gitversion_changelog || fatal "gitversion changelog mismatch"
+    _verify_gitver_matches_changelog || fatal "gitversion changelog mismatch"
 
     _ask "WARNING: about to push tag: $REQUESTED_TAG"
     git push github "$REQUESTED_TAG" || fatal "pushing tag failed"
 
     echo "https://github.com/alonswartz/notesium/actions"
     echo "https://github.com/alonswartz/notesium/releases"
+}
+
+main() {
+    case $1 in ''|-h|--help|help) usage;; esac
+    cd "$(dirname "$(dirname "$(realpath "$0")")")"
+
+    case $1 in
+        commits)    shift; print_commits $@;;
+        build)      shift; build_release $@;;
+        tag)        shift; tag_release $@;;
+        *)          fatal "unrecognized command: $1";;
+    esac
 }
 
 main "$@"
