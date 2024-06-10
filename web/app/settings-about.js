e4120892227b0ec4d33966dbef69a272c428a5a8
Alon Swartz <alon@alonswartz.org>
2024-06-10 13:12:54 +0300

    web/app: settings about - check for updates

--- web/app/settings-about.js
+++ web/app/settings-about.js
@@ -1,6 +1,41 @@
 var t = `
 <div class="h-full w-full overflow-y-scroll pb-6">
   <div class="relative flex-1 px-6 mt-2 space-y-6">
+
+    <div class="rounded-md border border-gray-200">
+      <div class="flex flex-wrap">
+        <dl class="flex w-full px-6 py-2 bg-gray-100 border-b border-gray-200 items-center justify-items-center justify-between">
+          <dt class="text-xs leading-6 text-gray-900 font-semibold">Software update</dt>
+          <dd v-if="versionCheck.inprogress" class="mt-1 text-blue-700"><Icon name="spinner-circle" size="h-4 w-4" /></dd>
+          <dd v-else class="text-xs leading-6 text-blue-700 hover:text-blue-600 hover:cursor-pointer" @click="checkVersion">Check for updates</dd>
+        </dl>
+        <dl class="px-6 py-2 flex w-full flex-none justify-between bg-gray-50">
+          <dt class="text-xs font-medium leading-6 text-gray-900">Current version</dt>
+          <dd class="text-xs font-mono leading-6 text-gray-900" v-text="versionInfo.version || 'unknown'"></dd>
+        </dl>
+        <dl class="px-6 py-2 flex w-full flex-none justify-between bg-gray-50">
+          <dt class="text-xs font-medium leading-6 text-gray-900">Latest release</dt>
+          <dd class="text-xs font-mono leading-6 text-gray-900" v-text="versionCheck.latest_version || 'unknown'"></dd>
+        </dl>
+        <dl class="px-6 py-2 flex w-full flex-none justify-between bg-gray-50">
+          <dt class="text-xs font-medium leading-6 text-gray-900">Last check</dt>
+          <dd class="text-xs font-mono leading-6 text-gray-900" v-text="formatDate(versionCheck.date) || 'unknown'"></dd>
+        </dl>
+      </div>
+      <div v-if="versionCheck.error" class="font-mono text-xs text-red-600 px-6 py-2" v-text="versionCheck.error"></div>
+      <div v-else-if="versionCheck.comparison == '-1'" class="w-full py-2 mx-auto border-t border-gray-200 text-center text-blue-700 bg-blue-50">
+        <a class="text-xs font-bold leading-6 hover:underline" target="_blank" href="https://github.com/alonswartz/notesium/releases">
+          A new release is available <span aria-hidden="true"> &rarr;</span>
+        </a>
+      </div>
+      <div v-else-if="versionCheck.comparison == '0'" class="w-full py-2 mx-auto border-t border-gray-200 text-center text-green-700 bg-green-50">
+        <span class="text-xs font-medium leading-6">You are using the latest version</span>
+      </div>
+      <div v-else-if="versionCheck.comparison == '1'" class="w-full py-2 mx-auto border-t border-gray-200 text-center text-yellow-700 bg-yellow-50">
+        <span class="text-xs font-medium leading-6">You are using a newer version than the latest release</span>
+      </div>
+    </div>
+
     <div class="rounded-md border border-gray-200">
       <dl class="flex flex-wrap">
         <div class="flex-auto pl-6 py-2 bg-gray-100 border-b border-gray-200">
@@ -38,6 +73,7 @@ var t = `
         </div>
       </dl>
     </div>
+
   </div>
 </div>
 `
@@ -48,9 +84,40 @@ export default {
   data() {
     return {
       versionInfo: {},
+      versionCheck: {},
     }
   },
   methods: {
+    checkVersion() {
+      this.versionCheck.error = '';
+      this.versionCheck.comparison = '';
+      this.versionCheck.latest_version = '';
+      this.versionCheck.inprogress = true;
+      fetch('/api/raw/version?verbose=true&check=true')
+        .then(r => r.ok ? r.text() : r.text().then(e => Promise.reject(e)))
+        .then(text => {
+          if (text.toLowerCase().startsWith('error')) {
+            this.versionCheck.error = text.trim();
+            return;
+          }
+          const lines = text.split('\n');
+          lines.forEach(line => {
+            const [key, ...rest] = line.split(':');
+            const value = rest.join(':').trim();
+            switch (key) {
+              case 'comparison': this.versionCheck.comparison = value; break;
+              case 'latest.version': this.versionCheck.latest_version = value; break;
+            }
+          });
+        })
+        .catch(e => {
+          this.versionCheck.error = e.Error;
+        })
+        .finally(() => {
+          this.versionCheck.inprogress = false;
+          this.versionCheck.date = new Date();
+        });
+    },
     getVersionInfo() {
       fetch('/api/raw/version?verbose=true')
         .then(r => r.ok ? r.text() : r.text().then(e => Promise.reject(e)))
@@ -63,6 +130,17 @@ export default {
         .catch(e => {
           console.log(e.Error);
         });
+    },
+    formatDate(date) {
+      if (date) {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+        const day = String(date.getDate()).padStart(2, '0');
+        const hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+        const seconds = String(date.getSeconds()).padStart(2, '0');
+        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+      }
     },
   },
   computed: {
