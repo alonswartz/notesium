fa1a800bd76f6d8bf90c60097469962a68b975b5
Alon Swartz <alon@alonswartz.org>
2024-06-25 12:49:32 +0300

    web/app: state - explicitly set defaults, ensure always present

--- web/app/state.js
+++ web/app/state.js
@@ -1,10 +1,18 @@
 const { reactive, watch } = Vue;
 
-const savedState = sessionStorage.getItem('notesiumState');
-const initialState = savedState ? JSON.parse(savedState) : {};
-export const notesiumState = reactive(initialState);
+const defaultState = {
+  showLabelsPanel: false,
+  showNotesPanel: false,
+  showNoteSidebar: true,
+};
 
-watch(notesiumState, (updatedState) => {
-  sessionStorage.setItem('notesiumState', JSON.stringify(updatedState));
+const savedState = sessionStorage.getItem('notesiumState');
+const initialState = savedState ? JSON.parse(savedState) : defaultState;
+const notesiumState = reactive({ ...defaultState, ...initialState });
+
+watch(notesiumState, (newState) => {
+  Object.assign(newState, { ...defaultState, ...newState });
+  sessionStorage.setItem('notesiumState', JSON.stringify(newState));
 }, { deep: true });
 
+export { notesiumState };
