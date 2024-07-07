3ca8b261f1303fba666e0ad443f7ff661ba7170b
Alon Swartz <alon@alonswartz.org>
2024-07-07 14:22:25 +0300

    web/app: sidepanel notes - scoped darkmode support

--- web/app/sidepanel.js
+++ web/app/sidepanel.js
@@ -1,8 +1,8 @@
 var t = `
 <Pane v-if="$notesiumState.showLabelsPanel" name="labelsPanel" :defaultWidth="195" :minWidth="100">
-  <div class="h-full overflow-y-auto bg-gray-800 text-gray-400 text-sm font-medium">
+  <div class="h-full overflow-y-auto bg-gray-800 text-gray-400 text-sm font-medium dark-scroll">
     <div title="notes with 1-word titles are considered labels"
-      class="flex items-center justify-items-center h-9 border-b border-gray-700 bg-gray-700">
+      class="flex items-center justify-items-center h-9 border-b border-gray-500">
       <input class="h-full w-full pl-4 text-white placeholder:text-gray-400 bg-transparent focus:outline-none text-sm"
         @keydown.space.prevent
         @keyup.esc="newLabel=''; $refs.newLabelInput.blur()"
@@ -29,7 +29,7 @@ var t = `
   </div>
 </Pane>
 
-<Pane v-if="$notesiumState.showNotesPanel" name="notesPanel" :defaultWidth="380" :minWidth="100" class="border-r border-gray-200">
+<Pane :class="{'dark': dark}" v-if="$notesiumState.showNotesPanel" name="notesPanel" :defaultWidth="380" :minWidth="100" class="border-r border-gray-200">
 
   <Transition
     enter-from-class="opacity-0"
@@ -47,40 +47,54 @@ var t = `
     </div>
   </Transition>
 
-  <div class="flex items-center justify-items-center h-9 border-b border-gray-200 bg-gray-100">
+  <div class="flex items-center justify-items-center h-9 border-b border-gray-200 bg-gray-100 dark:bg-gray-700 dark:border-gray-500">
     <input ref="queryInput" v-model="query" placeholder="filter..." autocomplete="off" spellcheck="false"
       @keyup.esc="query = ''; $refs.queryInput.blur();"
-      class="h-full w-full px-4 text-gray-900 placeholder:text-gray-400 bg-gray-100 ring-0 border-none focus:outline-none text-sm" />
+      class="h-full w-full px-4 ring-0 border-none focus:outline-none text-sm placeholder:text-gray-400
+             text-gray-900 bg-gray-100 dark:text-gray-100 dark:bg-gray-700" />
 
     <div class="inline-flex items-center justify-items-center mt-3 m-2 h-full space-x-4">
-      <div v-show="query" @click="query = ''" title="clear" class="-mt-1 pr-2 text-gray-400 hover:text-gray-700 cursor-pointer border-r border-gray-300">
+      <div v-show="query" @click="query = ''" title="clear"
+        class="-mt-1 pr-2 cursor-pointer border-r border-gray-300 text-gray-400 hover:text-gray-700 dark:border-gray-500 dark:hover:text-gray-300">
         <Icon name="mini-x-mark" size="h-5 w-5" />
       </div>
 
       <div class="relative group inline-block text-left">
-        <span title="labels" class="cursor-pointer text-gray-400 group-hover:text-gray-700">
+        <span title="labels" class="cursor-pointer text-gray-400 group-hover:text-gray-700 dark:group-hover:text-gray-200">
           <Icon name="outline-tag" size="h-5 w-5" class="pb-1" />
         </span>
         <div class="hidden group-hover:block absolute right-0 z-50 w-64 pt-3 -mt-1 origin-top-right">
           <div class="rounded-md bg-white shadow-md border border-gray-200">
-            <ul class="divide-y divide-gray-100 text-sm">
-              <li class="flex items-center justify-between p-2 cursor-pointer hover:bg-gray-50" @click="labelsSortBy='title'">
-                <span class="text-gray-600">Title</span>
-                <span v-show="labelsSortBy == 'title'" class="text-indigo-500"><Icon name="mini-check" size="h-5 w-5" /></span>
-              </li>
-              <li class="flex items-center justify-between p-2 cursor-pointer hover:bg-gray-50" @click="labelsSortBy='links'">
-                <span class="text-gray-600">Link count</span>
-                <span v-show="labelsSortBy == 'links'" class="text-indigo-500"><Icon name="mini-check" size="h-5 w-5" /></span>
-              </li>
-              <li class="bg-gray-200 pt-1"></li>
+            <ul class="text-sm divide-y divide-gray-100">
+              <template v-if="$notesiumState.showLabelsPanel || (dense && showLabelsTree)">
+                <li class="flex items-center justify-between p-2 cursor-pointer" @click="labelsSortBy='title'">
+                  <span class="text-gray-600">Title</span>
+                  <span v-show="labelsSortBy == 'title'" class="text-indigo-500"><Icon name="mini-check" size="h-5 w-5" /></span>
+                </li>
+                <li class="flex items-center justify-between p-2 cursor-pointer hover:bg-gray-50" @click="labelsSortBy='links'">
+                  <span class="text-gray-600">Link count</span>
+                  <span v-show="labelsSortBy == 'links'" class="text-indigo-500"><Icon name="mini-check" size="h-5 w-5" /></span>
+                </li>
+              </template>
+              <template v-else>
+                <li class="flex items-center justify-between p-2 text-gray-300">
+                  <span>Title</span>
+                  <span v-show="labelsSortBy == 'title'"><Icon name="mini-check" size="h-5 w-5" /></span>
+                </li>
+                <li class="flex items-center justify-between p-2 text-gray-300">
+                  <span>Link count</span>
+                  <span v-show="labelsSortBy == 'links'"><Icon name="mini-check" size="h-5 w-5" /></span>
+                </li>
+              </template>
+              <li class="pt-px bg-gray-200"></li>
               <li v-if="dense" class="flex items-center justify-between p-2 cursor-pointer hover:bg-gray-50"
                 @click="showLabelsTree=!showLabelsTree">
                 <span class="text-gray-600">Labels tree</span>
                 <span v-show="showLabelsTree" class="text-indigo-500"><Icon name="mini-check" size="h-5 w-5" /></span>
               </li>
-              <li v-else class="flex items-center justify-between p-2">
-                <span class="text-gray-300">Labels tree</span>
-                <span v-show="showLabelsTree" class="text-gray-300"><Icon name="mini-check" size="h-5 w-5" /></span>
+              <li v-else class="flex items-center justify-between p-2 text-gray-300">
+                <span>Labels tree</span>
+                <span v-show="showLabelsTree"><Icon name="mini-check" size="h-5 w-5" /></span>
               </li>
               <li class="flex items-center justify-between p-2 cursor-pointer hover:bg-gray-50"
                 @click="$notesiumState.showLabelsPanel=!$notesiumState.showLabelsPanel">
@@ -88,9 +102,9 @@ var t = `
                 <span v-show="$notesiumState.showLabelsPanel" class="text-indigo-500"><Icon name="mini-check" size="h-5 w-5" /></span>
               </li>
               <template v-if="!$notesiumState.showLabelsPanel">
-                <li class="bg-gray-200 pt-1"></li>
+                <li class="pt-px bg-gray-200"></li>
                 <li class="flex items-center justify-items-center p-2" title="notes with 1-word titles are considered labels">
-                  <input class="h-full w-full text-gray-800 placeholder:text-gray-400 bg-transparent focus:outline-none text-sm"
+                  <input class="h-full w-full bg-transparent focus:outline-none text-sm placeholder:text-gray-400 text-gray-800"
                     @keydown.space.prevent
                     @keyup.esc="newLabel=''; $refs.newLabelInput.blur()"
                     @keyup.enter="createNewLabelNote()"
@@ -108,7 +122,7 @@ var t = `
       </div>
 
       <div class="relative group inline-block text-left">
-        <span title="sort &amp; density" class="cursor-pointer text-gray-400 group-hover:text-gray-700">
+        <span title="sort &amp; density" class="cursor-pointer text-gray-400 group-hover:text-gray-700 dark:group-hover:text-gray-200">
           <Icon name="outline-bars-arrow-down" size="h-5 w-5" />
         </span>
         <div class="hidden group-hover:block absolute right-0 z-50 w-64 pt-3 -mt-1 origin-top-right">
@@ -130,7 +144,7 @@ var t = `
                 <span class="text-gray-600">Link count</span>
                 <span v-show="sortBy == 'links'" class="text-indigo-500"><Icon name="mini-check" size="h-5 w-5" /></span>
               </li>
-              <li class="bg-gray-200 pt-1"></li>
+              <li class="bg-gray-200 pt-px"></li>
               <li class="flex items-center justify-between p-2 cursor-pointer hover:bg-gray-50" @click="dense=true">
                 <span class="text-gray-600">Compact view</span>
                 <span v-show="dense" class="text-indigo-500"><Icon name="mini-check" size="h-5 w-5" /></span>
@@ -138,6 +152,11 @@ var t = `
               <li class="flex items-center justify-between p-2 cursor-pointer hover:bg-gray-50" @click="dense=false">
                 <span class="text-gray-600">Detailed view</span>
                 <span v-show="!dense" class="text-indigo-500"><Icon name="mini-check" size="h-5 w-5" /></span>
+              </li>
+              <li class="bg-gray-200 pt-px"></li>
+              <li class="flex items-center justify-between p-2 cursor-pointer hover:bg-gray-50" @click="dark=!dark">
+                <span class="text-gray-600">Dark mode</span>
+                <span v-show="dark" class="text-indigo-500"><Icon name="mini-check" size="h-5 w-5" /></span>
               </li>
             </ul>
           </div>
@@ -147,29 +166,29 @@ var t = `
     </div>
   </div>
 
-  <div class="h-full overflow-y-scroll">
+  <div class="h-full overflow-y-scroll dark:bg-gray-700" :class="{'dark-scroll': dark}">
 
     <ul v-if="dense" class="mt-2 text-sm">
       <li v-if="showLabelsTree" v-for="note in filteredLabelNotes" :key="'label-' + note.Filename">
         <details class="cursor-pointer [&_svg]:open:rotate-90">
           <summary class="group flex items-center justify-between justify-items-center list-none py-1.5 pl-2
-                          rounded-r-2xl text-gray-900 hover:bg-indigo-50 focus:outline-none">
+                          rounded-r-2xl focus:outline-none text-gray-900 hover:bg-indigo-50 dark:text-gray-400 dark:hover:bg-gray-600">
             <div class="flex items-center justify-center gap-x-2 truncate">
-              <div class="text-gray-400">
+              <div class="text-gray-400 dark:text-gray-500">
                 <Icon name="chevron-right" size="h-5 w-5" />
               </div>
               <span class="overflow-hidden truncate pr-2" v-text="note.Title" />
             </div>
-            <span class="hidden group-hover:block pr-2 text-gray-400 hover:text-gray-600" v-text="note.LinkCount + ' ↗'"
+            <span class="hidden group-hover:block pr-2 text-gray-400 hover:text-gray-600 dark:hover:text-gray-200" v-text="note.LinkCount + ' ↗'"
               @mouseenter="previewFilename=note.Filename" @mouseleave="previewFilename=''"
               @click.stop="$emit('note-open', note.Filename, 1)" />
           </summary>
-          <div v-if="note.LinkCount > 0" class="ml-[18px] border-dotted border-l border-gray-300">
+          <div v-if="note.LinkCount > 0" class="ml-[18px] border-dotted border-l border-gray-300 dark:border-gray-500">
             <div v-for="link in note.Links" :key="'link-' + link.Filename" @click="$emit('note-open', link.Filename, link.LineNumber)"
-              class="group flex items-center justify-items-center justify-between pl-[18px] py-1 pr-2 truncate text-gray-900
-                     rounded-r-2xl hover:bg-indigo-50">
+              class="group flex items-center justify-items-center justify-between pl-[18px] py-1 pr-2 truncate rounded-r-2xl
+                     text-gray-900 dark:text-gray-400 hover:bg-indigo-50 dark:hover:bg-gray-600">
               <div class="leading-6 overflow-hidden truncate" v-text="link.Title" :title="link.Title"></div>
-              <div class="hidden group-hover:block text-gray-400 hover:text-gray-600 -mr-1 pr-1"
+              <div class="hidden group-hover:block text-gray-400 hover:text-gray-600 dark:hover:text-gray-200 -mr-1 pr-1"
                 @mouseenter="previewFilename=link.Filename" @mouseleave="previewFilename=''" v-text="'↗'">
               </div>
             </div>
@@ -178,32 +197,33 @@ var t = `
       </li>
       <li v-for="note in filteredNotes" :key="note.Filename"
         @click="$emit('note-open', note.Filename)"
-        class="group flex justify-between py-1 pl-4 pr-2 cursor-pointer text-gray-900 rounded-r-2xl hover:bg-indigo-50">
+        class="group flex justify-between py-1 pl-4 pr-2 cursor-pointer rounded-r-2xl text-gray-900 hover:bg-indigo-50
+               dark:text-gray-400 dark:hover:bg-gray-600">
         <div class="text-sm leading-6 overflow-hidden truncate" v-text="note.Title" :title="note.Title"></div>
-        <div class="hidden group-hover:block text-gray-400 hover:text-gray-600 -mr-1 pr-1"
+        <div class="hidden group-hover:block text-gray-400 hover:text-gray-600 dark:hover:text-gray-200 -mr-1 pr-1"
           @mouseenter="previewFilename=note.Filename" @mouseleave="previewFilename=''" v-text="'↗'">
         </div>
       </li>
     </ul>
 
-    <ul v-else class="divide-y divide-gray-100">
+    <ul v-else class="divide-y divide-gray-100 dark:divide-gray-600">
       <li v-for="note in filteredNotes" :key="note.Filename"
         @click="$emit('note-open', note.Filename)"
-        class="group flex justify-between py-3 pl-4 pr-2 cursor-pointer hover:bg-gray-50">
+        class="group flex justify-between py-3 pl-4 pr-2 cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-600">
         <div class="truncate">
-          <div class="text-sm leading-6 text-gray-900 overflow-hidden truncate" v-text="note.Title" :title="note.Title"></div>
+          <div class="text-sm leading-6 overflow-hidden truncate text-gray-900 dark:text-gray-300" v-text="note.Title" :title="note.Title"></div>
           <div class="flex space-x-1 overflow-hidden truncate text-xs text-gray-400 leading-6">
             <span v-if="sortBy == 'ctime'" v-text="note.CtimeRelative" title="created" />
             <span v-else v-text="note.MtimeRelative" title="modified" />
             <div class="space-x-1 overflow-hidden truncate">
               <template v-for="label in note.Labels">
                 <span>·</span>
-                <span class="hover:text-gray-600" v-text="label" @click.stop="query='label:'+label+' '"></span>
+                <span class="hover:text-gray-600 dark:hover:text-gray-200" v-text="label" @click.stop="query='label:'+label+' '"></span>
               </template>
             </div>
           </div>
         </div>
-        <div class="hidden group-hover:block text-gray-400 hover:text-gray-600 -my-3 py-3 -mr-2 pr-2"
+        <div class="hidden group-hover:block text-gray-400 hover:text-gray-600 dark:hover:text-gray-200 -my-3 py-3 -mr-2 pr-2"
           @mouseenter="previewFilename=note.Filename" @mouseleave="previewFilename=''" v-text="'↗'">
         </div>
       </li>
@@ -223,10 +243,11 @@ export default {
   components: { Pane, Icon, Preview },
   data() {
     return {
+      dark: true,
       query: '',
       sortBy: 'title',
       labelsSortBy: 'title',
-      dense: true,
+      dense: false,
       showLabelsTree: true,
       notes: [],
       newLabel: '',
--- web/app/tailwind.config.js
+++ web/app/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*.js", "!vendor.js", "!tailwind.config.js"],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
