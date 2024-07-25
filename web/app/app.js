c97831aefadf9c8a413f77e1975b7d7fd18e6e51
Alon Swartz <alon@alonswartz.org>
2024-07-25 13:52:16 +0300

    web/app: graphpanel - trigger refresh per lastSave

--- web/app/app.js
+++ web/app/app.js
@@ -7,7 +7,7 @@ var t = `
   <SidePanel v-if="$notesiumState.showLabelsPanel || $notesiumState.showNotesPanel"
     :lastSave="lastSave" @note-open="openNote" @note-new="newNote" @finder-open="openFinder" />
 
-  <GraphPanel v-if="$notesiumState.showGraphPanel" :activeFilename=activeFilename @note-open="openNote" />
+  <GraphPanel v-if="$notesiumState.showGraphPanel" :lastSave=lastSave :activeFilename=activeFilename @note-open="openNote" />
 
   <div class="flex flex-col h-full w-full overflow-x-auto">
     <nav class="flex bg-gray-200 text-gray-800">
