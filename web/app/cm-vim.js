1ad6b63b3eaf4e1e1da456e10aa374ef86d327e2
Alon Swartz <alon@alonswartz.org>
2024-09-08 13:29:07 +0300

    web/app: vim-mode - handleClose with optional confirmIfModified

--- web/app/cm-vim.js
+++ web/app/cm-vim.js
@@ -1,5 +1,6 @@
 export function initCodeMirrorVimEx() {
   CodeMirror.Vim.defineEx('quit', 'q', function(cm, cmd) {
-    if (cm.quit) cm.quit();
+    const confirmIfModified = cmd.argString !== '!';
+    if (cm.quit) cm.quit(confirmIfModified);
   });
 }
