5b7e22732d0d80ff7db5ad8eab5e514368c37282
Alon Swartz <alon@alonswartz.org>
2024-09-10 16:11:56 +0300

    web/app: vim-mode - passthrough keypress configurable timeout

--- web/app/cm-vim.js
+++ web/app/cm-vim.js
@@ -36,15 +36,16 @@ export function initCodeMirrorVimEx(notesiumState) {
   CodeMirror.Vim.defineEx('BodyKeyEvent', '', (cm, cmd) => {
     const key = cmd.args[0];
     const code = cmd.args[1];
+    const timeout = parseInt(cmd.args[2], 10);
     const ctrlKey = key.startsWith('<C')
     cm.display.input.blur();
     document.body.focus();
     document.body.dispatchEvent(new KeyboardEvent('keydown', { key: key, code: code, ctrlKey: ctrlKey, bubbles: true, cancelable: true, composed: true }));
-    setTimeout(() => { cm.focus(); }, 2000)
+    (timeout > 0) ? setTimeout(() => { cm.focus(); }, timeout) : cm.focus();
   });
-  CodeMirror.Vim.map('<Space>', ':BodyKeyEvent <Space> Space', 'normal');
-  CodeMirror.Vim.map('<C-h>', ':BodyKeyEvent <C-h> KeyH', 'normal');
-  CodeMirror.Vim.map('<C-l>', ':BodyKeyEvent <C-l> KeyL', 'normal');
+  CodeMirror.Vim.map('<Space>', ':BodyKeyEvent <Space> Space 2000', 'normal');
+  CodeMirror.Vim.map('<C-h>', ':BodyKeyEvent <C-h> KeyH 0', 'normal');
+  CodeMirror.Vim.map('<C-l>', ':BodyKeyEvent <C-l> KeyL 0', 'normal');
 
   CodeMirror.Vim.defineOption('wrap', notesiumState.editorLineWrapping, 'boolean', [], (value, cm) => {
     if (cm) return; // option is global, do nothing for local
