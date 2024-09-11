6f57dde03c14d6436d6715c4bbd41f99038efe49
Alon Swartz <alon@alonswartz.org>
2024-09-11 15:42:48 +0300

    web/app: vim-mode - statusbar editor options

--- web/app/note-statusbar.js
+++ web/app/note-statusbar.js
@@ -1,7 +1,14 @@
 var t = `
-<div class="flex bg-gray-200 text-xs">
+<div class="flex justify-between bg-gray-200 text-xs">
   <span v-if="vimMode" class="pl-3 pr-4 pt-1.5 pb-1 uppercase font-semibold text-white rounded-r-full" :class="vimModeCls" v-text="vimModeText"></span>
   <span v-else class="pl-3 pr-4 pt-1.5 pb-1 uppercase text-gray-500">not focused</span>
+
+  <div class="flex space-x-5 px-4 items-center justify-items-center text-gray-500 bg-gray-300 rounded-l-full">
+    <span title="line wrapping" @click="$notesiumState.editorLineWrapping = !$notesiumState.editorLineWrapping"
+      class="cursor-pointer hover:text-gray-700" v-text="$notesiumState.editorLineWrapping ? 'wrap' : 'nowrap'" />
+    <span title="conceal formatting" @click="$notesiumState.editorConcealFormatting = !$notesiumState.editorConcealFormatting"
+      class="cursor-pointer hover:text-gray-700" v-text="$notesiumState.editorConcealFormatting ? 'conceal' : 'noconceal'" />
+  </div>
 </div>
 `
 
