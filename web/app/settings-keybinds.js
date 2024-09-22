4ed6fbbfbe660d0933538a8451d517439cbd1b4c
Alon Swartz <alon@alonswartz.org>
2024-09-22 12:02:34 +0300

    web/app: settings - keybinds restructure for consistency and clarity

--- web/app/settings-keybinds.js
+++ web/app/settings-keybinds.js
@@ -1,20 +1,20 @@
 var t = `
 <div class="h-full w-full overflow-y-scroll pb-6">
-  <div class="relative flex-1 px-6">
-    <div class="relative group flex items-center justify-items-center justify-between space-x-2 pb-2 border-b border-gray-200">
-      <input ref="queryInput" v-model="query" autofocus placeholder="keybindings search..." autocomplete="off" spellcheck="false"
-        @blur="$refs.queryInput && $refs.queryInput.focus()"
-        class="h-12 bg-gray-100 w-full border-0 my-2 rounded-lg px-4 text-gray-900 placeholder:text-gray-400 ring-0 focus:outline-none text-sm" />
-    </div>
-    <dl class="mt-2 space-y-2 divide-y divide-gray-100 text-xs">
-      <div v-for="keybind in filteredItems" class="pt-2 sm:flex items-center">
-        <dt class="font-medium text-gray-900 hover:underline cursor-pointer sm:w-16 flex-none" v-text="keybind[0]" @click="query=keybind[0]"></dt>
-        <dd class="flex justify-between gap-x-6 items-center sm:flex-auto">
-          <span class="text-gray-600" v-text="keybind[3]"></span>
-          <span class="text-gray-900 font-mono bg-gray-200 rounded-md px-2 pt-2 pb-1" v-text="keybind[2]"></span>
-        </dd>
+  <div class="relative flex-1 px-6 mt-2 space-y-6">
+    <div v-for="section in sections" :key="section.name" class="rounded-md border border-gray-200">
+      <div class="pl-6 py-2 bg-gray-100 border-b border-gray-200 items-center justify-items-center">
+        <span class="text-xs font-semibold leading-6 text-gray-900" v-text="section.title"></span>
       </div>
-    </dl>
+      <div class="divide-y divide-gray-100 w-full">
+        <div v-for="entry in section.entries" class="w-full flex px-6 py-2 text-xs items-center justify-items-center justify-between">
+          <div class="flex">
+            <span v-if="entry[2]" class="text-gray-600 font-medium w-16 mt-1" v-text="entry[2]"></span>
+            <span class="text-gray-900 font-medium mt-1" v-text="entry[1]"></span>
+          </div>
+          <span class="text-gray-900 font-mono bg-gray-200 rounded-md p-2 pb-1" v-text="entry[0]"></span>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 `
@@ -22,54 +22,66 @@ var t = `
 export default {
   data() {
     return {
-      query: '',
-      keybinds: [
-        ['global', 'note-new', 'space n n', 'Open new note for editing'],
-        ['global', 'note-daily', 'space n d', 'Open new or existing daily note'],
-        ['global', 'note-weekly', 'space n w', 'Open new or existing weekly note'],
-        ['global', 'finder-open-list-default', 'space n l', 'Open list with prefixed label, sorted alphabetically'],
-        ['global', 'finder-open-list-ctime', 'space n c', 'Open list with prefixed date created, sorted by ctime'],
-        ['global', 'finder-open-list-mtime', 'space n m', 'Open list with prefixed date modified, sorted by mtime'],
-        ['global', 'finder-open-links', 'space n k', 'Open list of related notes to open note, or all note links if none open'],
-        ['global', 'finder-open-search', 'space n s', 'Open full text search'],
-        ['global', 'graph-open', 'space n g', 'Open force graph view'],
-        ['finder', 'finder-entry-down', 'ctrl j', 'Select next entry (down)'],
-        ['finder', 'finder-entry-up', 'ctrl k', 'Select previous entry (up)'],
-        ['finder', 'finder-entry-select', 'enter', 'Submit selected entry'],
-        ['finder', 'finder-preview-toggle', 'ctrl p', 'Toggle preview'],
-        ['finder', 'finder-dismiss', 'esc', 'Dismiss finder'],
-        ['note', 'note-insert-link', '[[', 'Insert note link from note list selection'],
-        ['note', 'note-save', 'ctrl s', 'Save note'],
-        ['note', 'note-focus', 'tab', 'Insert editing mode'],
-        ['note', 'note-unfocus', 'esc', 'Exit editing mode'],
-        ['note', 'note-indent-auto', 'shift tab', 'Auto-indent current line or selection'],
-        ['note', 'note-indent-more', 'ctrl ]', 'Indent current line or selection'],
-        ['note', 'note-indent-less', 'ctrl [', 'Dedent current line or selection'],
-        ['table', 'note-table-format-advance', 'tab', 'Format table and advance column (right)'],
-        ['table', 'note-table-left', 'shift tab', 'Navigate to previous column (left)'],
-        ['table', 'note-table-navigate', 'alt arrow', 'Navigate rows and columns'],
-        ['tab', 'tab-left', 'ctrl h', 'Switch to previous tab (left)'],
-        ['tab', 'tab-right', 'ctrl l', 'Switch to next tab (right)'],
-        ['tab', 'tab-previous', 'ctrl 6', 'Switch to previously active tab'],
-      ]
+      sections: [
+        {
+          name: 'global',
+          title: 'Global',
+          entries: [
+            ['space n n', 'Open new note for editing'],
+            ['space n d', 'Open new or existing daily note'],
+            ['space n w', 'Open new or existing weekly note'],
+            ['space n l', 'Open list with prefixed label, sorted alphabetically'],
+            ['space n c', 'Open list with prefixed date created, sorted by ctime'],
+            ['space n m', 'Open list with prefixed date modified, sorted by mtime'],
+            ['space n k', 'Open list of related notes to active note, or all note links if none open'],
+            ['space n s', 'Open full text search'],
+            ['space n g', 'Open force graph view'],
+          ]
+        },
+        {
+          name: 'finder',
+          title: 'Finder',
+          entries: [
+            ['ctrl p', 'Toggle preview'],
+            ['ctrl j', 'Select next entry (down)'],
+            ['ctrl k', 'Select previous entry (up)'],
+            ['enter', 'Submit selected entry'],
+            ['esc', 'Dismiss finder'],
+          ]
+        },
+        {
+          name: 'tabs',
+          title: 'Note tabs',
+          entries: [
+            ['ctrl h', 'Switch to previous tab (left)'],
+            ['ctrl l', 'Switch to next tab (right)'],
+            ['ctrl 6', 'Switch to previously active tab'],
+          ]
+        },
+        {
+          name: 'editor-default',
+          title: 'Editor: Default mode',
+          entries: [
+            ['tab', 'Enter editing mode (focus active note)', 'none'],
+            ['ctrl s', 'Save note', 'all'],
+            ['[[', 'Insert note link from note list selection', 'edit'],
+            ['shift tab', 'Auto-indent current line or selection', 'edit'],
+            ['ctrl ]', 'Indent current line or selection', 'edit'],
+            ['ctrl [', 'Dedent current line or selection', 'edit'],
+            ['esc', 'Exit editing mode (unfocus)', 'edit'],
+          ]
+        },
+        {
+          name: 'editor-table',
+          title: 'Editor: Table formatting and navigation',
+          entries: [
+            ['tab', 'Format table and advance column (right)', 'table'],
+            ['shift tab', 'Navigate to previous column (left)', 'table'],
+            ['alt arrow', 'Navigate rows and columns', 'table'],
+          ]
+        },
+      ],
     }
-  },
-  computed: {
-    filteredItems() {
-      const queryWords = this.query.toLowerCase().split(' ');
-      return !this.query
-        ? this.keybinds
-        : this.keybinds.filter(item => 
-            queryWords.every(queryWord => 
-              item[0].toLowerCase().includes(queryWord) || 
-              item[1].toLowerCase().includes(queryWord) ||
-              item[3].toLowerCase().includes(queryWord)
-            )
-          );
-    },
-  },
-  created() {
-    this.$nextTick(() => { this.$refs.queryInput.focus(); });
   },
   template: t
 }
